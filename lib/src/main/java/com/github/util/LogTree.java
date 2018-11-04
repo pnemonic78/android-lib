b2a1c80f17682ee955b21879c322903ea7f3287a
Moshe <pnemonic@gmail.com>
2018-11-04 09:34:49 +0200

    logger

--- lib/src/main/java/com/github/util/LogTree.java
+++ lib/src/main/java/com/github/util/LogTree.java
@@ -15,36 +15,31 @@
  */
 package com.github.util;
 
+import android.util.Log;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
 import timber.log.Timber;
 
 /**
- * Logger utilities.
+ * Logger tree for Timber.
  *
  * @author Moshe Waisberg
  */
-public class LogUtils {
+public class LogTree extends Timber.DebugTree {
 
-    public static void e(String tag, String msg, Throwable e) {
-        Timber.e(e, msg);
+    private final boolean release;
+
+    public LogTree(boolean debug) {
+        this.release = !debug;
     }
 
-    public static void e(String tag, Throwable e) {
-        Timber.e(e);
-    }
-
-    public static void e(String tag, String msg) {
-        Timber.e(msg);
-    }
-
-    public static void i(String tag, String msg) {
-        Timber.i(msg);
-    }
-
-    public static void v(String tag, String msg) {
-        Timber.v(msg);
-    }
-
-    public static void w(String tag, String msg) {
-        Timber.w(msg);
+    @Override
+    protected void log(int priority, @Nullable String tag, @NotNull String message, @Nullable Throwable t) {
+        if (release && ((priority == Log.DEBUG) || (priority == Log.VERBOSE))) {
+            return;
+        }
+        super.log(priority, tag, message, t);
     }
 }
