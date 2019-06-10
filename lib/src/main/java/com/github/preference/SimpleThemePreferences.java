6955650f53ebef385dc18b7eb048ffc8ef0fbfa0
Moshe Waisberg <moshe@playstudios-il.com>
2019-06-10 17:21:15 +0300

    daynight - default theme

--- lib/src/main/java/com/github/preference/SimpleThemePreferences.java
+++ lib/src/main/java/com/github/preference/SimpleThemePreferences.java
@@ -67,6 +67,20 @@ public class SimpleThemePreferences extends SimplePreferences implements ThemePr
         if (THEME_LIGHT.equals(value)) {
             return R.style.Theme_Base_Light;
         }
+        if (THEME_DEFAULT.equals(value)) {
+            final int nightMode = context.getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK;
+            switch (nightMode) {
+                case Configuration.UI_MODE_NIGHT_NO:
+                    return R.style.Theme_Base_Light;
+                case Configuration.UI_MODE_NIGHT_YES:
+                    return R.style.Theme_Base;
+            }
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                // Material
+                return R.style.Theme_Base_Light;
+            }
+        }
         return R.style.Theme_Base;
     }
 
@@ -81,17 +95,15 @@ public class SimpleThemePreferences extends SimplePreferences implements ThemePr
             return false;
         }
         if (THEME_DEFAULT.equals(value)) {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                if (BuildCompat.isAtLeastQ()) {
-                    final int nightMode = context.getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK;
-                    switch (nightMode) {
-                        case Configuration.UI_MODE_NIGHT_NO:
-                            return false;
-                        case Configuration.UI_MODE_NIGHT_YES:
-                            return true;
-                    }
-                }
+            final int nightMode = context.getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK;
+            switch (nightMode) {
+                case Configuration.UI_MODE_NIGHT_NO:
+                    return false;
+                case Configuration.UI_MODE_NIGHT_YES:
+                    return true;
+            }
 
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
                 // Material
                 return false;
             }
