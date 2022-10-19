222226ddd06baacee2c349bb421ed9f5116c1379
Moshe Waisberg <Moshe.Waisberg@nike.com>
2022-10-19 13:08:19 +0300

    New permission to show notifications

--- lib/src/main/java/com/github/app/AppExtensions.kt
+++ lib/src/main/java/com/github/app/AppExtensions.kt
@@ -1,10 +1,10 @@
 package com.github.app
 
+import android.annotation.TargetApi
 import android.app.PendingIntent
 import android.os.Build
-import androidx.annotation.RequiresApi
 
-@RequiresApi(api = Build.VERSION_CODES.M)
+@TargetApi(Build.VERSION_CODES.M)
 @JvmField
 val PendingIntent_FLAG_IMMUTABLE =
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) PendingIntent.FLAG_IMMUTABLE else 0
