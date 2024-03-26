8df40ad03be7e55d6922c5141ef96174a18de81b
M. Waisberg <pnemonic@gmail.com>
2024-03-26 09:19:20 +0200

    GeoNames country polygon

--- kotlin/src/main/kotlin/com/github/math/MathExt.kt
+++ kotlin/src/main/kotlin/com/github/math/MathExt.kt
@@ -11,3 +11,29 @@ fun Float.toDegrees(): Float = this.toDouble().toDegrees().toFloat()
 val Int.isEven get() = this.and(1) == 0
 
 val Int.isOdd get() = this.and(1) == 1
+
+fun sqr(a: Double): Double = a * a
+
+operator fun IntArray.times(value: Int): IntArray {
+    return IntArray(size) { i -> this[i] * value }
+}
+
+operator fun IntArray.times(value: Double): IntArray {
+    return IntArray(size) { i -> (this[i] * value).toInt() }
+}
+
+operator fun IntArray.timesAssign(value: Int) {
+    for (i in indices) {
+        this[i] *= value
+    }
+}
+
+operator fun IntArray.timesAssign(value: Double) {
+    for (i in indices) {
+        this[i] = (this[i] * value).toInt()
+    }
+}
+
+fun IntArray.toDoubleArray(): DoubleArray {
+    return DoubleArray(size) { i -> this[i].toDouble() }
+}
